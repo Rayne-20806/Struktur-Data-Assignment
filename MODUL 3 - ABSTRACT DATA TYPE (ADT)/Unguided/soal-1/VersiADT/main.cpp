8b873891782f73e9391567f6f9414fb07219d380
Ryan Muhamad Saputra <ryan.saputra20806@gmail.com>
2025-10-16 16:52:44 +0700

    Add main function to handle Mahasiswa data

--- MODUL 3 - ABSTRACT DATA TYPE (ADT)/Unguided/soal-1/VersiADT/main.cpp
+++ MODUL 3 - ABSTRACT DATA TYPE (ADT)/Unguided/soal-1/VersiADT/main.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+#include "mahasiswa.h" // Sertakan ADT sebelumnya
+using namespace std;
+
+int main() {
+    Mahasiswa mhs[MAX_MHS]; 
+    int jumlahMhs = 0;           
+
+  
+    inputData(mhs, jumlahMhs);
+    tampilkanData(mhs, jumlahMhs);
+
+    return 0;
+}
