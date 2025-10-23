0275e7b051fe49524111a73219078a1194067512
Ryan Muhamad Saputra <ryan.saputra20806@gmail.com>
2025-10-23 23:54:59 +0700

    Update main.cpp

--- MODUL 5 - SINGLY LINKED LIST (BAGIAN KEDUA) (5)/Guided/main.cpp
+++ MODUL 5 - SINGLY LINKED LIST (BAGIAN KEDUA) (5)/Guided/main.cpp
@@ -1,1 +1,59 @@
+#include "listBuah.h"
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    linkedlist List;
+    address nodeA, nodeB, nodeC, nodeD, nodeE = Nil;
+    createList(List);
+
+    dataBuah dtbBuah;
+
+    nodeA = alokasi("Jeruk", 100, 3000);
+    nodeB = alokasi("Apel", 75, 4000);
+    nodeC = alokasi("pir", 87, 5000);
+    nodeD = alokasi("Semangka", 43, 11500);
+    nodeE = alokasi("Durian", 15, 314500);
+   
+
+    insertFirst(List, nodeA);
+    insertLast(List, nodeB);
+    insertAfter(List, nodeC, nodeA);
+    insertAfter(List, nodeD, nodeC);
+    insertLast(List, nodeE);
+
+    cout << "--- ISI LIST SETELAH DILAKUKAN INSERT ---" << endl;
+    printList(List);
+    //lanjutan  
+    cout << "Jumlah Node: " << nbList(List) << endl;
+    cout << endl;
+
+
+    cout << "--- ISIS LIST SETELAH DILAKUKAN UPDATE --- " << endl;
+    printList(List);
+    cout << "Jumlah node: " << nbList(List) << endl;
+    cout << endl;
+
+    FindNodeByData(List, "Kelapa");
+    FindNodeByAddress(List, nodeC);
+    FindNodeByRange(List, 5000, 10000);
+    
+    delFirst(List);
+    delLast(List);
+    delAfter(List, nodeD, nodeC);
+
+    cout << "--- ISI LIST SETELAH DILAKUKAN DELETE ---" << endl;
+    printList(List);
+    cout << "Jumlah Node: " << nbList(List) << endl;
+    cout << endl;
+    
+    deleteList(List);
+    cout << "--- ISI LIST SETELAH DILAKUKAN DELETE ---" << endl;
+    printList(List);
+    cout << "Jumlah Node: " << nbList(List) << endl;
+    cout << endl;
+}
+
+//note: jika ingin hapus list maka lepas dulu pointer previousnya agar bisa hapus node
+// Untuk Laprak Unguided....hindara Data Buah dan Data Mahasiswa
